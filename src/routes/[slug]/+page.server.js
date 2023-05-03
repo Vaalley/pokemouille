154812fbee10cda5aae11de48297d3a2c094e677
Vaalley <vaaleyou@gmail.com>
2023-05-03 19:47:14 +0200

    Todo, fix the page not refreshing properly

--- src/routes/[slug]/+page.server.js
+++ src/routes/[slug]/+page.server.js
@@ -1,14 +1,27 @@
+import { GraphQLClient } from 'graphql-request';
 import NodeCache from 'node-cache';
 
 const cache = new NodeCache({ stdTTL: 21600, checkperiod: 120 }); // 21600 = 6 hours stdTTL
+
+const client = new GraphQLClient('https://beta.pokeapi.co/graphql/v1beta', {
+	headers: {
+		'Content-Type': 'application/json',
+		'Accept': '*/*'
+	},
+});
 
 export async function load({ params }) {
 	const { slug } = params;
 
 	try {
 		const pokemon = await getPokemon(slug);
+		const pokemonList = await getAllPokemon();
+
+		// console.log(pokemonList);
+
 		return {
-			pokemon,
+			pokemonInfo: pokemon,
+			pokemon: pokemonList,
 			status: 200,
 		};
 	} catch (error) {
@@ -38,4 +51,44 @@ async function getPokemon(slug) {
 	cache.set(slug, data);
 
 	return data;
+}
+
+async function getAllPokemon() {
+	let pokemon = cache.get('pokemon');
+	if (pokemon) {
+		console.log('cache hit');
+		return pokemon;
+	}
+	console.log('cache miss, fetching pokemon');
+	pokemon = await fetchAllPokemon();
+	cache.set('pokemon', pokemon);
+	return pokemon;
+}
+
+async function fetchAllPokemon() {
+	const countData = await fetch("https://pokeapi.co/api/v2/pokedex/1/").then(res => res.json());
+	const count = countData.pokemon_entries.length - 2;
+	try {
+		const query = `
+		query allPokemon {
+			pokemon_v2_pokemon(order_by: {id: asc}, limit: ${count}) {
+				name
+				id
+				pokemon_v2_pokemonsprites {
+					sprites
+				}
+				pokemon_v2_pokemontypes_aggregate {
+					nodes {
+						pokemon_v2_type {
+							name
+						}
+					}
+				}
+			}
+		}		
+`;
+		return await client.request(query);
+	} catch (error) {
+		throw new Error('Error fetching pokemon data');
+	}
 }
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -3,54 +3,57 @@
 	import { capitalize, hyphenRemover } from '$lib/utils';
 	import SearchBar from '../../components/SearchBar.svelte';
 
-	let pokemon = data.pokemon;
+	let pokemonInfo = data.pokemonInfo;
+	let searchbarData = data;
 
-	// console.log(pokemon);
+	// console.log(searchbarData);
 </script>
 
 <svelte:head>
-	<title>{capitalize(pokemon.name)}</title>
+	<title>{capitalize(pokemonInfo.name)}</title>
 </svelte:head>
 
 <div class="flex flex-col items-center">
-	<h1 class="text-7xl text-center mt-8 mb-16 underline font-medium">{capitalize(pokemon.name)}</h1>
+	<h1 class="text-7xl text-center mt-8 mb-16 underline font-medium">
+		{capitalize(pokemonInfo.name)}
+	</h1>
 	<ul>
 		<img
 			class=" h-60"
-			src={pokemon.sprites.other['official-artwork'].front_default}
-			alt={pokemon.name}
+			src={pokemonInfo.sprites.other['official-artwork'].front_default}
+			alt={pokemonInfo.name}
 		/>
 	</ul>
 
 	<div class="flex text-2xl font-medium">
 		<ul>
 			<li class="underline">Height and Weight:</li>
-			<li>Height: {pokemon.height / 10} m</li>
-			<li>Weight: {pokemon.weight / 10} kg</li>
+			<li>Height: {pokemonInfo.height / 10} m</li>
+			<li>Weight: {pokemonInfo.weight / 10} kg</li>
 		</ul>
 
 		<ul>
 			<li class="underline">Types:</li>
-			{#each pokemon.types as type}
+			{#each pokemonInfo.types as type}
 				<li>{capitalize(type.type.name)}</li>
 			{/each}
 		</ul>
 
 		<ul>
 			<li class="underline">Abilities:</li>
-			{#each pokemon.abilities as ability}
+			{#each pokemonInfo.abilities as ability}
 				<li>{hyphenRemover(capitalize(ability.ability.name))}</li>
 			{/each}
 		</ul>
 
 		<ul>
 			<li class="underline">Stats:</li>
-			{#each pokemon.stats as stat}
+			{#each pokemonInfo.stats as stat}
 				<li>{hyphenRemover(capitalize(stat.stat.name))}: {stat.base_stat}</li>
 			{/each}
 		</ul>
 	</div>
-	<SearchBar {data} />
+	<SearchBar data={searchbarData} />
 </div>
 
 <style>
