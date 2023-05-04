011228cdb335e8d86b0e7adfb4ba906958dc3f75
Vaalley <vaaleyou@gmail.com>
2023-05-04 18:31:48 +0200

    Improved cache speed? test 1

--- src/lib/cache.js
+++ src/lib/cache.js
@@ -3,16 +3,20 @@ import { fetchAllPokemon } from '$lib/pokeapi.js';
 
 const cache = new NodeCache({ stdTTL: 21600, checkperiod: 120 }); // 21600 = 6 hours stdTTL
 
-async function getPokemon() {
-	let pokemon = cache.get('pokemon');
-	if (pokemon) {
-		console.log('cache hit');
-		return pokemon;
-	}
-	console.log('cache miss, fetching pokemon');
-	pokemon = await fetchAllPokemon();
-	cache.set('pokemon', pokemon);
-	return pokemon;
-}
+const getPokemon = (() => {
+	let memo = {};
+	return async () => {
+		const cached = cache.get('pokemon');
+		if (cached) {
+			console.log('cache hit');
+			return cached;
+		}
+		console.log('cache miss, fetching pokemon');
+		const result = await fetchAllPokemon();
+		memo = { pokemon: result };
+		cache.set('pokemon', memo.pokemon);
+		return memo.pokemon;
+	};
+})();
 
 export { getPokemon };
