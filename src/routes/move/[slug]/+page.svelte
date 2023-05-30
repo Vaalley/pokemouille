1484f2ce809c4bbae176b8ec8054a713aaefc895
Vaalley <vaaleyou@gmail.com>
2023-05-30 17:21:16 +0200

    Added more info

--- src/routes/move/[slug]/+page.svelte
+++ src/routes/move/[slug]/+page.svelte
@@ -1,10 +1,12 @@
 <script>
 	export let data;
-	import { capitalize, hyphenRemover, pokemonTypes, getTextColor } from '$lib/utils';
+	import { capitalize, hyphenRemover, pokemonTypes, getTextColor, getIdFromUrl } from '$lib/utils';
 	import SearchBar from '../../../components/SearchBar.svelte';
 
 	let moveInfo = data.moveInfo;
 	let searchData = data.searchData;
+	const pokemonSpritesBaseUrl =
+		'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
 
 	// Get the English flavor text
 	const englishFlavorText = moveInfo.flavor_text_entries.find(
@@ -15,6 +17,8 @@
 	// Get the English effect entry
 	const englishEffect = moveInfo.effect_entries.find((entry) => entry.language.name === 'en');
 	moveInfo.effect_entries = englishEffect ? [englishEffect] : [];
+
+	// console.log(moveInfo);
 </script>
 
 <svelte:head>
@@ -69,6 +73,24 @@
 				{:else}
 					<p class="text-gray-700">Data not available yet</p>
 				{/if}
+			</div>
+			<div class="my-8">
+				<h2 class="text-2xl font-semibold mb-2 text-gray-800">
+					List of Pokémon that can learn that move:
+				</h2>
+				<ul class="grid grid-cols-3 gap-4">
+					{#each moveInfo.learned_by_pokemon as pokemon}
+						<li class="flex flex-col items-center justify-center text-center">
+							<img
+								src={`${pokemonSpritesBaseUrl}/${getIdFromUrl(pokemon.url)}.png`}
+								alt={pokemon.name}
+								class="mb-2 w-20 h-20"
+								onerror="this.onerror=null; this.src='https://i.pinimg.com/736x/3f/53/20/3f5320bda51f29d0e6ef7a61d030c234--cricut.jpg'"
+							/>
+							<p class="text-gray-700">{pokemon.name}</p>
+						</li>
+					{/each}
+				</ul>
 			</div>
 			<SearchBar data={searchData} />
 		</div>
