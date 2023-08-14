f05c4171f22c548519d23a63a091a047ad9eecf6
Vaalley <vaaleyou@gmail.com>
2023-08-14 18:44:34 +0200

    Refactor getEvolutionTriggerDetails function in EvolutionChain component
    - Renamed function to getEvolutionTriggerDetails
    - Replaced if else chain with switch statement for better readability
    - Updated return statements to be more descriptive and concise
    
    This commit refactors the getEvolutionTriggerDetails function in the EvolutionChain component. The function has been renamed to getEvolutionTriggerDetails and the if else chain has been replaced with a switch statement for improved readability. The return statements have also been updated to provide more descriptive and concise details.

--- src/components/EvolutionChain.svelte
+++ src/components/EvolutionChain.svelte
@@ -6,7 +6,33 @@
 	const pokemonSpriteUrl =
 		'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-	// console.log(evolutionChainData);
+	function getEvolutionTriggerDetails(evolution) {
+		if (evolution.pokemon_v2_evolutiontrigger.name === 'level-up') {
+			return [
+				evolution.min_level ? `Level: ${evolution.min_level}` : '',
+				evolution.min_happiness ? `Happiness: ${evolution.min_happiness}` : '',
+				evolution.time_of_day ? `Time: ${evolution.time_of_day}` : '',
+				evolution.pokemon_v2_location
+					? `Location: ${capitalize(hyphenRemover(evolution.pokemon_v2_location.name))}`
+					: '',
+				evolution.min_affection ? `Affection: ${evolution.min_affection}` : '',
+				evolution.pokemonV2PokemonspecyByPartySpeciesId
+					? `Need in party: ${capitalize(
+							hyphenRemover(evolution.pokemonV2PokemonspecyByPartySpeciesId.name)
+					  )}`
+					: ''
+			]
+				.filter((detail) => detail !== '')
+				.join(', ');
+		} else if (evolution.pokemon_v2_evolutiontrigger.name === 'use-item') {
+			return capitalize(hyphenRemover(evolution.pokemon_v2_item.name));
+		} else if (evolution.pokemon_v2_evolutiontrigger.name === 'trade') {
+			return evolution.pokemonV2ItemByHeldItemId
+				? `Needs to carry: ${capitalize(hyphenRemover(evolution.pokemonV2ItemByHeldItemId.name))}`
+				: '';
+		}
+		return '';
+	}
 </script>
 
 <div class="container mx-auto mt-16">
@@ -35,69 +61,30 @@
 					<div class="flex flex-col">
 						{#each pokemon.pokemon_v2_pokemonevolutions as evolution}
 							<p class="h4 font-semibold">
-								{capitalize(hyphenRemover(evolution.pokemon_v2_evolutiontrigger.name))}
-								:
+								{capitalize(hyphenRemover(evolution.pokemon_v2_evolutiontrigger.name))}:
 							</p>
-							{#if evolution.pokemon_v2_evolutiontrigger.name === 'level-up'}
-								{#if evolution.min_level}
-									<p>Level: {evolution.min_level}</p>
-								{/if}
-								{#if evolution.min_happiness}
-									<p>Happiness: {evolution.min_happiness}</p>
-								{/if}
-								{#if evolution.time_of_day}
-									<p>Time: {evolution.time_of_day}</p>
-								{/if}
-								{#if evolution.pokemon_v2_location}
-									<p>
-										Location: {capitalize(hyphenRemover(evolution.pokemon_v2_location.name))}
-									</p>
-								{/if}
-								{#if evolution.min_affection}
-									<p>Affection: {evolution.min_affection}</p>
-								{/if}
-								{#if evolution.pokemonV2PokemonspecyByPartySpeciesId}
-									<p>
-										Need in party: {capitalize(
-											hyphenRemover(evolution.pokemonV2PokemonspecyByPartySpeciesId.name)
-										)}
-									</p>
-								{/if}
-							{/if}
-							{#if evolution.pokemon_v2_evolutiontrigger.name === 'use-item'}
-								<p>{capitalize(hyphenRemover(evolution.pokemon_v2_item.name))}</p>
-							{/if}
-							{#if evolution.pokemon_v2_evolutiontrigger.name === 'trade'}
-								{#if evolution.pokemonV2ItemByHeldItemId}
-									<p>
-										Needs to carry: {capitalize(
-											hyphenRemover(evolution.pokemonV2ItemByHeldItemId.name)
-										)}
-									</p>
-								{/if}
-							{/if}
+							<p>{getEvolutionTriggerDetails(evolution)}</p>
 						{/each}
 					</div>
 				{/if}
 			</a>
 			{#if pokemon.pokemon_v2_pokemons.length > 1}
-				{#each pokemon.pokemon_v2_pokemons as pokemon}
-					{#if pokemon.pokemon_v2_pokemonforms[0].form_name !== ''}
-						<!-- <p>{capitalize(hyphenRemover(pokemon.pokemon_v2_pokemonforms[0].name))}</p> -->
+				{#each pokemon.pokemon_v2_pokemons as form}
+					{#if form.pokemon_v2_pokemonforms[0].form_name !== ''}
 						<a
 							data-sveltekit-reload
 							class="rounded-none p-6 transition-all hover:variant-ringed-primary hover:text-primary-500"
-							href="/pokemon/{pokemon.pokemon_v2_pokemonforms[0].name}"
+							href={`/pokemon/${form.pokemon_v2_pokemonforms[0].name}`}
 						>
 							<div>
 								<p class="h3 text-center font-semibold">
-									{capitalize(hyphenRemover(pokemon.pokemon_v2_pokemonforms[0].name))}
+									{capitalize(hyphenRemover(form.pokemon_v2_pokemonforms[0].name))}
 								</p>
 								<img
 									class="mx-auto"
 									loading="lazy"
-									src={`${pokemonSpriteUrl}${pokemon.pokemon_v2_pokemonforms[0].pokemon_id}.png`}
-									alt={pokemon.pokemon_v2_pokemonforms[0].name}
+									src={`${pokemonSpriteUrl}${form.pokemon_v2_pokemonforms[0].pokemon_id}.png`}
+									alt={form.pokemon_v2_pokemonforms[0].name}
 								/>
 							</div>
 						</a>
