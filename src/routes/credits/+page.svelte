609ef3fe166c83651434b84fcdac84e96d64ab8e
Vaalley <vaaleyou@gmail.com>
2023-09-01 22:40:13 +0200

    Refine graphical assets credits in Credits page.
    - Add the links to Partywhale's repository that contains recreated Pokemon type icons as SVGs.
    - Add the links to PokéAPI's repository that provides every Pokémon sprites.
    
    These changes refine the graphical assets credits in the Credits page, providing users with easy access to the repositories where they can find the resources.

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -18,18 +18,36 @@
 	<div class="flex h-32 items-center justify-center gap-6">
 		<h1 class="h1 flex items-center gap-3 font-bold">🎉 Credits</h1>
 	</div>
-	<div class="container mx-auto grid grid-cols-2 items-baseline">
-		<!-- Item flavor text -->
-		<div class="mt-20">
-			<p>something</p>
+	<div class="container mx-auto mt-20 grid grid-cols-2 items-baseline">
+		<!-- Graphical Assets Credits -->
+		<div>
+			<h1 class="h1 font-bold">Graphical Assets Credits:</h1>
+			<div class="mt-5">
+				<h2 class="h2 font-semibold">
+					<a class="anchor" href="https://github.com/partywhale">Partywhale</a>
+				</h2>
+				<p class="h5 mt-2">
+					Partywhale recreated every Pokemon type icon as a SVG's, can be found here: <a
+						class="anchor"
+						href="https://github.com/partywhale/pokemon-type-icons"
+						>https://github.com/partywhale/pokemon-type-icons</a
+					>
+				</p>
+			</div>
+			<div class="mt-5">
+				<h2 class="h2 font-semibold">
+					<a class="anchor" href="https://github.com/PokeAPI">PokéAPI</a>
+				</h2>
+				<p class="h5 mt-2">
+					PokéAPI provides every Pokémon sprites in a github repo accessible for all, which can be
+					found here: <a class="anchor" href="https://github.com/PokeAPI/sprites"
+						>https://github.com/PokeAPI/sprites</a
+					>
+				</p>
+			</div>
 		</div>
-		<!-- Item effect -->
-		<div class="mt-12">
-			<p>something</p>
+		<div>
+			<!-- <h1 class="h1 font-bold">Other Credits:</h1> -->
 		</div>
-	</div>
-	<!-- Item category -->
-	<div class="container mx-auto mt-20">
-		<p>something</p>
 	</div>
 </div>
