510a9443f2a7ac73854e410ea07351265e187328
vaalley <vaaleyou@gmail.com>
2023-09-21 13:19:51 +0200

    Added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<main class="container mx-auto flex min-h-screen flex-col items-center justify-center">
+	<h1 class="h1">{$page.status}: {$page.error.message}</h1>
+	<p>Something went wrong on our server. We're working on it!</p>
+</main>
