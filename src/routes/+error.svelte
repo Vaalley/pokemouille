cfaac66526e21d8181fdceb8e65c3ebbe9e30ca0
Vaalley <vaaleyou@gmail.com>
2024-01-13 15:00:52 +0100

    Enhance error page UI
    Improve user guidance on error page by adding a card layout and a link
    to check external API status, enhancing clarity and support for users
    facing issues potentially caused by external dependencies.

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,17 @@
 	import { page } from '$app/stores';
 </script>
 
-<main class="container mx-auto flex min-h-screen flex-col items-center justify-center">
-	<h1 class="h1">{$page.status}: {$page.error.message}</h1>
-	<p>Something went wrong on our server. We're working on it!</p>
+<main class="card container mx-auto flex min-h-screen flex-col items-center justify-center">
+	<div class="card p-5 text-center">
+		<h1 class="h1">{$page.status}: {$page.error.message}</h1>
+		<p>Something went wrong on our server. We're working on it!</p>
+		<p>
+			It may be an issue from PokeAPI, you can check their server status here: <a
+				class="anchor"
+				href="https://pokeapi.statuspage.io/"
+				target="_blank"
+				rel="noopener noreferrer">PokeAPI</a
+			>
+		</p>
+	</div>
 </main>
