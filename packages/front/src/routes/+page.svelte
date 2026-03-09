88bb1a9fe13a6d0c14948a646d5cc8ffab21de6f
Valentin <valentinmusset@pm.me>
2026-03-09 11:21:20 +0100

    chore: clean up configuration files and remove project specs

--- packages/front/src/routes/+page.svelte
+++ packages/front/src/routes/+page.svelte
@@ -1,26 +1,2 @@
-<script lang="ts">
-  import { Accordion } from "bits-ui";
-</script>
- 
-
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<Accordion.Root type="single">
-  <Accordion.Item value="item-1">
-    <Accordion.Header>
-      <Accordion.Trigger>Item 1 Title</Accordion.Trigger>
-    </Accordion.Header>
-    <Accordion.Content
-      >This is the collapsible content for this section.</Accordion.Content
-    >
-  </Accordion.Item>
-  <Accordion.Item value="item-2">
-    <Accordion.Header>
-      <Accordion.Trigger>Item 2 Title</Accordion.Trigger>
-    </Accordion.Header>
-    <Accordion.Content
-      >This is the collapsible content for this section.</Accordion.Content
-    >
-  </Accordion.Item>
-</Accordion.Root>
